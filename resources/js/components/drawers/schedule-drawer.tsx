5990f9e656c8c0335fa71a85ace718ac54d9d0d3
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-03-06 09:25:42 +0800

    feat: adding editable in schedule

--- resources/js/components/drawers/schedule-drawer.tsx
+++ resources/js/components/drawers/schedule-drawer.tsx
@@ -29,15 +29,23 @@ export function ScheduleDrawer({ schedule, ...props }: React.ComponentPropsWitho
                     </DrawerDescription>
 
                 </DrawerHeader>
-                <div className="p-5 ">
+                <div className="p-10 ">
                     <DrawerDescription className="flex flex-col">
                         <span> {schedule.extendedProps.date} </span>
                         <span>   <span>Start from :</span>
                             {schedule.extendedProps.start_time} - {schedule.extendedProps.end_time}</span>
                     </DrawerDescription>
                 </div>
-                <DrawerFooter className="pt-2">
-                    <DrawerClose asChild>
+                <DrawerFooter className="pt-2  ">
+                    <div className="flex  mb-5  gap-5 w-full">
+                        <Button variant={'default'}>
+                            Edit
+                        </Button>
+                        <Button variant={'destructive'}>
+                            Delete
+                        </Button>
+                    </div>
+                    <DrawerClose className="self-end" asChild>
                         <Button variant="outline">Cancel</Button>
                     </DrawerClose>
                 </DrawerFooter>
--- resources/js/components/scheduler/calendar-scheduler.tsx
+++ resources/js/components/scheduler/calendar-scheduler.tsx
@@ -8,28 +8,29 @@ import { useState } from 'react'
 
 export function CalendarScheduler({ schedules }: { schedules: Schedule[] }) {
 
-    const [open,setOpen] =useState<boolean>(false);
-    const [selectedSchedule,setSelectedSchedule] =useState<Schedule | null>(null);
+    const [open, setOpen] = useState<boolean>(false);
+    const [selectedSchedule, setSelectedSchedule] = useState<Schedule | null>(null);
     const handleDateClick = (arg: any) => {
         setOpen(true);
         setSelectedSchedule(arg.event as Schedule);
 
     }
-
-
     return (
         <div className='p-5'>
-
             <FullCalendar
+
 
                 headerToolbar={{
 
                     left: 'prev,next today',
-                    center: 'title',
-                    right: ''
+                    right: 'title',
+                    // right: ''
                 }}
+                editable
+                eventDrop={(e)=>console.log(e)}
                 height={'90vh'}
-
+                // contentHeight={200}
+                expandRows
                 plugins={[dayGridPlugin, interactionPlugin]}
                 // initialView='dayGridMonth'
                 events={schedules}
