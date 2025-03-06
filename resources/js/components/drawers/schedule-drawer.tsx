a878ab09b1b71af569fb1f13c11b64567ec582b0
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-03-06 13:51:45 +0800

    feat: schedule drawer

--- resources/js/components/drawers/schedule-drawer.tsx
+++ resources/js/components/drawers/schedule-drawer.tsx
@@ -17,14 +17,14 @@ import { useForm } from "@inertiajs/react";
 
 export function ScheduleDrawer({ schedule, ...props }: React.ComponentPropsWithoutRef<typeof Drawer> & { schedule: Schedule | EventImpl }) {
 
-const form = useForm({});
+    const form = useForm({});
     return (
         <Drawer {...props}>
 
             <DrawerContent className="">
 
                 <DrawerHeader className="text-left">
-                    <DrawerTitle>{schedule.title}</DrawerTitle>
+                    <DrawerTitle className="space-x-1"><span>{schedule.title} </span>( {<span> {schedule.extendedProps.date} </span>})</DrawerTitle>
                     <DrawerDescription>
                         {schedule.extendedProps.description}
                     </DrawerDescription>
@@ -32,15 +32,15 @@ const form = useForm({});
                 </DrawerHeader>
                 <div className="p-10 ">
                     <DrawerDescription className="flex flex-col">
-                        <span> {schedule.extendedProps.date} </span>
+
                         <span>   <span>Start from :</span>
                             {schedule.extendedProps.start_time} - {schedule.extendedProps.end_time}</span>
                     </DrawerDescription>
                 </div>
                 <DrawerFooter className="pt-2  ">
                     <div className="flex  mb-5  gap-5 w-full">
-                        <Button  variant={'default'} onClick={()=>{
-                            console.log(schedule.id);
+                        <Button variant={'default'} onClick={() => {
+
                             form.get(route('schedules.edit', { schedule: schedule.id }), { preserveScroll: true });
 
 
