38535cc57cb623b3222faf8d9a09b67057b1f060
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-03-09 01:24:58 +0800

    validation requests for subject, sections,yealevels done

--- app/Http/Requests/UpdateSectionRequest.php
+++ app/Http/Requests/UpdateSectionRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateSectionRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateSectionRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,8 @@ class UpdateSectionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', Rule::unique('sections')->ignore($this->section)],
+
         ];
     }
 }
--- app/Http/Requests/UpdateSubjectRequest.php
+++ app/Http/Requests/UpdateSubjectRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateSubjectRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateSubjectRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,8 @@ class UpdateSubjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', Rule::unique('subject')->ignore($this->subject)],
+
         ];
     }
 }
