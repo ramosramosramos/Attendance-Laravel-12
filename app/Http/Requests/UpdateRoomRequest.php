5954214f3aec1afb2201035c55e4f33e2835e598
ramosramosramos <ramosramosramosramos12@gmail.com>
2025-03-10 12:23:17 +0800

    updated: room requestForms

--- app/Http/Requests/UpdateRoomRequest.php
+++ app/Http/Requests/UpdateRoomRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateRoomRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UpdateRoomRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,7 @@ class UpdateRoomRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name'=>['required', 'string',Rule::unique('rooms')->ignore($this->room)],
         ];
     }
 }
